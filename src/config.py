cb6a647c559d92155514efab4c11625e3a410f68
James Tan <jamestansx@gmail.com>
2021-03-04 21:01:03 +0800

    Update config.py
    Add chrome profile path setup

--- src/config.py
+++ src/config.py
@@ -6,11 +6,13 @@ import jsonfile
 appname = "WhatsMail"
 appauthor = "jamestansx"
 dirs = ""
-userDict = {}
+userData_json = ""
+userConfig_json = ""
 
 
 def isFirstRun():
     global dirs
+    global userData_json
     dirs = setting.getDirs(appname, appauthor)
     userData_json = os.path.join(dirs["userData"], "data.json")
     if os.path.isfile(userData_json):
@@ -20,6 +22,7 @@ def isFirstRun():
 
 
 def isConfig():
+    global userConfig_json
     userConfig_json = os.path.join(dirs["userConfig"], "config.json")
     if os.path.isfile(userConfig_json):
         return getConfig(userConfig_json)
@@ -30,19 +33,32 @@ def isConfig():
 def setupSetting(pathToFile):
     download_path = input("Please enter the download path: ")
     webdriverPath = input("Please enter the path to the Chrome webdriver: ")
-    jsonfile.write_json(pathToFile, writeSettings(download_path, webdriverPath))
-    global userDict
-    userDict = createKey(pathToFile)
-    return download_path, webdriverPath
+    chromedataPath = getChomedataPath()
+    pathDict = {
+        "downloadPath": download_path,
+        "webdriverPath": webdriverPath,
+        "chromedataPath": chromedataPath,
+    }
+    jsonfile.write_json(pathToFile, writeSettings(pathDict))
+    return download_path, webdriverPath, chromedataPath
 
+def getChomedataPath():
+    response = input("Chrome profile path: \nDefault path (y/n): ")
+    if response.strip().lower() in {'y', 'yes'}:
+        userCachedProfile = os.path.join(dirs["userCache"], "websession")
+        os.makedirs(userCachedProfile, exist_ok=True)
+        return userCachedProfile
+    if response.strip().lower() in {'n', 'no'}:
+        chromedataPath = input("Please enter the path to the Chrome appdata: ")
+        return chromedataPath
 
 def setupConfig(pathToFile):
     target = setting.target()
     username, gmail = target.newTarget()
     targetDict = {"username": username, "gmail": gmail}
+    userDict = createKey()
     jsonfile.write_json(pathToFile, writeConfig(targetDict, userDict))
     return username, gmail, userDict["userGmail"]
-
 
 def getSettings(pathToFile):
     data = jsonfile.read_json(pathToFile)
@@ -51,24 +67,28 @@ def getSettings(pathToFile):
             return setupSetting(pathToFile)
         if data["keyStatus"] is False:
             global userDict
-            userDict = createKey(pathToFile)
+            userDict = createKey()
+            jsonfile.write_json(userConfig_json, writeConfig(None, userDict))
     except Exception as e:
         raise e
     path = data["path"]
-    downloadPath, webdriverPath = None, None
+    downloadPath, webdriverPath, chromedataPath = None, None, None
     for key in path:
         if os.path.isfile(path[key]) is True:
             if key in "downloadPath":
                 downloadPath = path[key]
             if key in "webdriverPath":
                 webdriverPath = path[key]
-    return downloadPath, webdriverPath
+        if os.path.isdir(path[key]) is True:
+            if key in "chromedataPath":
+                chromedataPath = path[key]
+    return downloadPath, webdriverPath, chromedataPath
 
 
 def getConfig(pathToFile):
     data = jsonfile.read_json(pathToFile)
     try:
-        if data["target"] is not None and data["user"] is not None:
+        if  bool(data["target"])  and bool(data["user"]):
             for key in data["target"]:
                 username = key["username"]
                 gmail = key["gmail"]
@@ -81,33 +101,32 @@ def getConfig(pathToFile):
         raise e
 
 
-def writeSettings(download_path, webdriverPath):
+def writeSettings(pathDict):
 
     data = {}
     data["isFirstRun"] = False
     data["keyStatus"] = False
-    data["path"] = {}
-    path = data["path"]
-    path["downloadPath"] = download_path
-    path["webdriverPath"] = webdriverPath
+    data["path"] = pathDict
     return data
 
 
-def writeConfig(targetDict, userDict):
+def writeConfig(targetDict = None, userDict = None):
 
     data = {}
-    data["target"] = []
     data["user"] = []
-    data["target"].append(targetDict)
-    data["user"].append(userDict)
+    data["target"] = []
+    if userDict is not None:
+        data["user"].append(userDict)
+    if targetDict is not None:
+        data["target"].append(targetDict)
     return data
 
 
-def createKey(pathToFile):
+def createKey():
     key = setting.Key()
     key_status = key.keyGen()
-    data = jsonfile.read_json(pathToFile)
+    data = jsonfile.read_json(userData_json)
     data["keyStatus"] = key_status
-    jsonfile.updata_json(pathToFile, data)
+    jsonfile.updata_json(userData_json, data)
     userDict = {"userGmail": key.userGmailAcc}
     return userDict
