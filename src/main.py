03d45b42399ac9fe97bcf9995880a7001bd16f6d
James Tan <jamestansx@gmail.com>
2021-03-04 17:19:01 +0800

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import config
+import websetup
+import openmessages
+import fetchMessages
+import sendEmail
+
+target_username, gmail, download_path, webdriverPath, userGmail = "", "", "", "", ""
+
+
+def presetup():
+    global target_username, gmail, download_path, webdriverPath, userGmail
+
+    download_path, webdriverPath = config.isFirstRun()
+
+    target_username, gmail, userGmail = config.isConfig()
+
+    return websetup.open_whatsapp(download_path, webdriverPath)
+
+
+def main():
+    if openmessages.open_conversation(target_username) is True:
+        message_list = fetchMessages.fetchMessages()
+        sendEmail.sendGmail(userGmail, target_username, gmail, message_list, download_path)
+
+
+if __name__ == "__main__":
+    if presetup() is True:
+        main()
