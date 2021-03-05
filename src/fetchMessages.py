f50c809119f23a52023acc8656d87d296346b549
James Tan <jamestansx@gmail.com>
2021-03-05 14:44:23 +0800

    Update fetchMessages.py
    Add checking if file has finished downloading

--- src/fetchMessages.py
+++ src/fetchMessages.py
@@ -1,9 +1,13 @@
 from selenium.webdriver.support.ui import WebDriverWait
-
+import time
 import websetup
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+FileIsFounded = False
 
 
-def fetchMessages():
+def fetchMessages(downloadPath):
     messages_class = websetup.driver.find_element_by_xpath("//div[@class='_11liR']")
     messages_elem_list = messages_class.find_elements_by_xpath(
         ".//div[contains(@class,'focusable-list-item')]"
@@ -21,7 +25,7 @@ def fetchMessages():
                 messageList.append(message)
             except Exception:
                 try:
-                    download(message)
+                    download(message, downloadPath)
                 except Exception as e:
                     print(e)
                     pass
@@ -37,46 +41,75 @@ def getMessage(element):
     return message
 
 
-def download(message):
+def download(message, downloadPath):
     try:
-        download_pdf(message)
+        download_pdf(message, downloadPath)
     except Exception:
         try:
-            download_Img(message)
+            download_Img(message, downloadPath)
         except Exception as e:
             print(e)
 
 
-def download_Img(message):
+def download_Img(message, downloadPath):
     image_container = message.find_element_by_xpath('.//div[contains(@role,"button")]')
     image_container.click()
-    img_download_button_container = websetup.driver.find_elements_by_xpath(
-        "//div[contains(@class,'_2n-zq')]"
-    )
-    for img in img_download_button_container:
-        try:
-            img_download_button = img.find_element_by_xpath(
-                './/span[contains(@data-icon, "download")]'
-            )
-            img_download_button.click()
-        except Exception:
-            pass
-        finally:
-            exit_img(img_download_button)
-
-
-def exit_img(img_download_button):
     try:
-        exit_container = websetup.driver.find_element_by_xpath(
-            "//div[contains(@class, '_1ljzS _2Ryya')]"
+        img_download_button = websetup.driver.find_element_by_xpath(
+            '//*[@id="app"]/div/span[3]/div/div/div[2]/div[1]/div[2]/div/div[4]/div/span'
         )
-        exit = exit_container.find_element_by_xpath(".//div[contains(@title, 'Close')]")
+        img_download_button.click()
+    except Exception:
+        pass
+    finally:
+        exit_img(downloadPath, img_download_button)
+
+
+def exit_img(downloadPath, img_download_button):
+    try:
+        exit = websetup.driver.find_element_by_xpath(
+            "//*[@id='app']/div/span[3]/div/div/div[2]/div[1]/div[2]/div/div[5]/div/span"
+        )
         exit.click()
         WebDriverWait(websetup.driver, websetup.longdelay).until_not(lambda x: img_download_button)
+        watch = OnMyWatch(downloadPath)
+        watch.run()
     except Exception:
         pass
 
 
-def download_pdf(message):
+def download_pdf(message, downloadPath):
     pdf = message.find_element_by_xpath('.//div[contains(@class,"MYzfD")]')
     pdf.click()
+    watch = OnMyWatch(downloadPath)
+    watch.run()
+
+
+class OnMyWatch:
+    # Set the directory on watch
+    def __init__(self, downloadPath):
+        self.observer = Observer()
+        self.watchDirectory = downloadPath
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.watchDirectory, recursive=True)
+        self.observer.start()
+        while FileIsFounded is not True:
+            time.sleep(2)
+        self.observer.stop()
+        print("Observer Stopped")
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == "created":
+            # Event is created, you can process it now
+            global FileIsFounded
+            print(f"{event.src_path}")
+            FileIsFounded = True
